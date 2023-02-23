5dca6214f7b451d27bd92d1aab0eee9e8e9e20a7
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2023-02-23 12:50:03 +0300

    Seminar1 finish

---
+++ C_sharp_Lections/Seminar1/4th_Task/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число");
+int Number = Convert.ToInt32(Console.ReadLine());
+for (int a = Number*(-1); a <= Number; a++)
+{
+    Console.WriteLine(a);
+}
---
+++ C_sharp_Lections/Seminar1/5th_Task/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int Number = Convert.ToInt32(Console.ReadLine());
+if (Number > 99 && Number < 1000)
+{
+    Console.WriteLine(Number%10);
+}
+else 
+{ 
+    Console.WriteLine("Вы ввели не трехзначное число!");
+}
