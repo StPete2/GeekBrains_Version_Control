d40a5da56a513c488c9138d59856dcc8d5b90ffe
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2023-02-25 16:18:28 +0300

    end of the day

---
+++ C_sharp_Lections/Examples/Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,45 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index]= new Random().Next(1,10);
+        index = index + 1;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position] + " ");
+        position = position + 1;
+    }
+}
+int IndexF(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; //если элемента нет, то он выдаст значение -1!!! Круто!!!
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index = index + 1;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+int find = new Random().Next(0,10);
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine("Ищем индекс элемента со значением " + find);
+int pos = IndexF(array, find);
+Console.WriteLine(pos);
