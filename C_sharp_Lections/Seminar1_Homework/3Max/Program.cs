ba5e14d00dff0fd6def3f93f73a43472a46617b1
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2023-02-26 10:59:40 +0300

    second hour of work

---
+++ C_sharp_Lections/Seminar1_Homework/3Max/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое целое число");
+int FirstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int SecondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье целое число");
+int ThirdNumber = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (SecondNumber > max)
+{
+    SecondNumber = max;
+}
+else if (ThirdNumber > max)
+{
+    ThirdNumber = max;
+}
+Console.WriteLine();
+Console.WriteLine("Максимальное число из трех: " + max);
---
+++ C_sharp_Lections/Seminar1_Homework/MinMax/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое целое число");
+int FirstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int SecondNumber = Convert.ToInt32(Console.ReadLine());
+if (FirstNumber > SecondNumber)
+{
+    Console.WriteLine("Первое число больше второго.");
+    Console.WriteLine(FirstNumber + " - это большее, а " + SecondNumber + " - это меньшее.");   
+}
+else
+{
+    Console.WriteLine("Второе число больше первого.");
+    Console.WriteLine(SecondNumber + " - это большее, а " + FirstNumber + " - это меньшее.");
+}
