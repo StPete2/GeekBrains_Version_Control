4cf588c5a386bf3dfc855a54c92583b91fe2b2f6
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2023-02-28 19:31:16 +0300

    part 1

--- C_sharp_Lections/Seminar2_Homework/ThirdNumber/Program.cs
+++ C_sharp_Lections/Seminar2_Homework/ThirdNumber/Program.cs
@@ -3,11 +3,14 @@ int number = Convert.ToInt32(Console.ReadLine());
 if (number/100 == 0)
 {
     Console.WriteLine("Вы ввели однозначное или двухзначное число, а именно: " + number);
+    Console.WriteLine("Третьей цифры в этом числе нет.");
 }
-// else if ()
-// {
-    
-// }
+else if (number/1000!=0)
+{
+    int firstinterim = number%1000;
+    int thirddigit = firstinterim/100;
+    Console.WriteLine("Третьей цифрой в числе {1} является {0}", thirddigit, number);
+}
 else
 {
     int thirddigit = number/100;
