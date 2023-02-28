23941fc48c11966a27da23b9070cbf8d0f93423c
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2023-02-28 19:07:36 +0300

    first two

---
+++ C_sharp_Lections/Seminar2_Homework/ThirdNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number/100 == 0)
+{
+    Console.WriteLine("Вы ввели однозначное или двухзначное число, а именно: " + number);
+}
+// else if ()
+// {
+    
+// }
+else
+{
+    int thirddigit = number/100;
+    Console.WriteLine("Третьей цифрой в числе {1} является {0}", thirddigit, number);
+}
