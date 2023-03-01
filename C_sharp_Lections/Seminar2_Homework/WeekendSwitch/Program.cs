3edf55ad25cfac87e15c4ab6db590889d646d7c6
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2023-03-01 10:24:53 +0300

    finish homework

---
+++ C_sharp_Lections/Seminar2_Homework/WeekendSwitch/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите номер дня недели");
+int number = Convert.ToInt32(Console.ReadLine());
+switch (number)
+{
+    case 1:
+    Console.WriteLine("День является рабочим");
+    break;
+    case 2:
+    Console.WriteLine("День является рабочим");
+    break;
+    case 3:
+    Console.WriteLine("День является рабочим");
+    break;
+    case 4:
+    Console.WriteLine("День является рабочим");
+    break;
+    case 5:
+    Console.WriteLine("День является рабочим");
+    break;
+    case 6:
+    Console.WriteLine("Выходной день!");
+    break;
+    case 7:
+    Console.WriteLine("Выходной день!");
+    break;
+    default:
+    Console.WriteLine("Такого дня недели не существует. Введите цифру от 1 до 7");
+    break;
+}
