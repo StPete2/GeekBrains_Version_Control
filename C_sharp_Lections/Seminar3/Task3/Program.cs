94932bd122fdc7f5d9eb8171cf6f059b4e9beb0e
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2023-03-02 12:09:25 +0300

    seminar3 finsih

---
+++ C_sharp_Lections/Seminar3/Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите координаты первой точки");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты второй точки");
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+double result = Math.Sqrt(Math.Pow(x1-x2, 2)+Math.Pow(y1-y2, 2));
+Console.WriteLine($"Расстояние равно {Math.Round(result, 2)}");
+// double number = 27;
+// Console.WriteLine(Math.Sqrt(number));
+Math.Pow(2,4);
