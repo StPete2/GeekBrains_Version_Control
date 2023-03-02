ddd19979cf631e8884658758447fdcfb976c99f3
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2023-03-02 10:46:01 +0300

    first two

---
+++ C_sharp_Lections/Seminar3/Points_task1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача №18. Работа в группах
+// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите номер четверти");
+int number = Convert.ToInt32(Console.ReadLine());
+switch (number) 
+{
+    case 1: 
+    {
+        Console.WriteLine("Х > 0, Y > 0");
+        break;
+    }
+    case 2:
+    {   
+        Console.WriteLine("X < 0, Y > 0");
+        break;
+    }
+    case 3: 
+    {
+        Console.WriteLine("X < 0, Y < 0");
+        break;
+    }
+    case 4: 
+    {
+        Console.WriteLine("X > 0, Y < 0");
+        break;
+    }
+    default: 
+    {
+        Console.WriteLine("Введите значение от 1 до 4");
+        break;
+    }
+}
---
+++ C_sharp_Lections/Seminar3/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача №22. Работа в группах
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
+// от 1 до N.
+//Math.Pow()
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write($"{number} -> ");
+for (int index = 1; index <= number; index++)
+{
+    Console.Write($"{index*index} ");
+}
