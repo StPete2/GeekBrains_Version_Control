9ab6840eb34edce84fb2a72157aacdfbefb62ff9
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2023-03-07 18:42:01 +0300

    first ready

---
+++ C_sharp_Lections/Seminar4_Homework/TwoNumbersPowerFunction/Program.cs
@@ -0,0 +1,20 @@
+﻿int InputNumber()
+{
+    Console.WriteLine("Введите степень");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Random()
+{
+    int number = new Random().Next(5,10);
+    return number;
+}
+
+double Power(int number, int power)
+{
+    double result = Math.Pow(number, power);
+    return result;
+}
+int a = Random();
+int b = InputNumber();
+Console.WriteLine($"Случайное число {a}, возведенное в степень {b} равно {Power(a,b)}");
