9d6f57dde2c476e5dd189fd8acb9c4b34f4d5032
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2023-03-11 12:48:26 +0300

    first part of the day

---
+++ C_sharp_Lections/SamFolder/BubbleSorting/Program.cs
@@ -0,0 +1,18 @@
+﻿int [] array = {8,7,6,10,1,4,3,2,9,0,1};
+Console.WriteLine(String.Join(" ", array));
+int sortedSize = 0;
+for (int j = 0; j < array.Length; j++)
+{
+    for (int i = 0; i < array.Length - 1 - sortedSize; i++)
+    {
+        if (array[i] > array [i+1])
+        {
+            int temp = array[i];
+            array[i] = array[i+1];
+            array[i+1] = temp;
+        }
+        Console.WriteLine(String.Join(" ", array));
+    }
+    sortedSize ++;    
+}
+Console.WriteLine(String.Join(" ", array));
---
+++ C_sharp_Lections/SamFolder/SortingArrayMin1/Program.cs
@@ -0,0 +1,27 @@
+﻿//Сортировка массива на первое место минимальное число
+int[] array = {8,6,3,2,1,5,4,7,1};
+void PrintArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+PrintArray(array);
+
+for (int i = 0; i < array.Length - 1; i++)
+{
+    int minPosition = i;
+    for (int j = i + 1; j < array.Length; j++)
+    {
+        if (array[j] < array [minPosition])
+        {
+            minPosition = j;
+        }
+    }
+    int temp = array[i];
+    array[i] = array[minPosition];
+    array[minPosition] = temp;
+}
+PrintArray(array);
