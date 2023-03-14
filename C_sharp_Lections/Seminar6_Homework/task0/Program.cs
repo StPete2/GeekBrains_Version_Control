56320b108bd57ca7d678a7be11ccf12c3f728ab5
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2023-03-14 16:25:44 +0300

    finish of the day

---
+++ C_sharp_Lections/Seminar6_Homework/task0/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число или числа");
+string input = Console.ReadLine();
+char [] charArray = input.ToCharArray();
+Console.WriteLine(String.Join("", charArray));
+// int result;
+// for (int i = 0; i < charArray.Length; i++)
+// {
+//     if (int.TryParse(charArray[i], out result))
+//     {
+//         Console.Write(result + " ");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Чисел нет");
+//     }
+// }
