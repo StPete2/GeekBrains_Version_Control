8476932df9f20139d4849fd97f3a0904437f7bea
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2023-03-16 15:13:38 +0300

    first part of the homework

--- C_sharp_Lections/Seminar6_Homework/task1/Program.cs
+++ C_sharp_Lections/Seminar6_Homework/task1/Program.cs
@@ -1,12 +1,26 @@
 ﻿// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
-double y = k1*x + b1;
-double y2 = k2*x + b2;
-// if (y == y2)
-// {
-//     Console.WriteLine($"Прямые пересекаются в точках {x} и {y}");
-// }
-// else if ()
-// {
-    
-// }
+// https://www.cyberforum.ru/turbo-pascal/thread691145.html
+const int COEFFICIENT = 0;
+const int CONSTANT = 1;
+const int LINE1 = 1;
+const int LINE2 = 2;
+
+
+double Prompt(string text)
+{
+    Console.WriteLine(text);
+    string value = Console.ReadLine();
+    return Convert.ToDouble(value);
+}
+
+double [] InputLineData(int numberOfLine)
+{
+    double [] lineData = new double [2];
+    lineData[COEFFICIENT] = Prompt($"Введите коэффициент для {numberOfLine} прямой");
+    lineData[CONSTANT] = Prompt($"Введите константу для {numberOfLine} прямой");
+    return lineData;
+}
+
+double [] lineData1 = InputLineData(LINE1);
+double [] lineData2 = InputLineData(LINE2);
