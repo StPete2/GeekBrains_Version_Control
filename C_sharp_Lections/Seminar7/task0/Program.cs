9c888e774e4a2e5f9103a243ad4c00934bba1c38
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2023-03-16 11:25:01 +0300

    first commit

---
+++ C_sharp_Lections/Seminar7/task0/Program.cs
@@ -0,0 +1,27 @@
+﻿// https://www.cyberforum.ru/turbo-pascal/thread691145.html
+// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными 
+// целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+int [,] CreateOutputArray(int m, int n)
+{
+    int [,] array = new int [m,n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(1,11);
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+    return array;
+}
+
+// foreach (int el in CreateArray(3,4))
+// {
+//     Console.Wrie(el + " ");
+// }
+CreateOutputArray(3,4);
---
+++ C_sharp_Lections/Seminar7/task2/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса
+// чётные, и замените эти элементы на их квадраты.
+// Например, изначально массив выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+int Prompt(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int [,] CreateOutputArray(int m, int n)
+{
+    int [,] array = new int [m,n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(1,11);
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    return array;
+}
+int m = Prompt("Введите количество строк");
+int n = Prompt("Введите количество столбцов");
+
+//CreateOutputArray(m,n);
+Console.WriteLine();
+int [,] array = CreateOutputArray(m,n);
+
+int [,] MultiplyEvenIndex (int [,] array)
+{
+    // int [,] newArray = new int [m,n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i %2 ==0 && j %2 == 0)
+            {
+                array[i,j] = array[i,j]*array[i,j];
+                // Console.Write(array[i,j] + "\t");
+            }
+            // else 
+            // {   
+            //     array[i,j] = array[i,j];
+            Console.Write(array[i,j] + "\t");
+            // }
+        }
+        Console.WriteLine();
+    }
+    return array;
+}
+Console.WriteLine();
+int [,] newArray = MultiplyEvenIndex(array);
