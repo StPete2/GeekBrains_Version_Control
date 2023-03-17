9e66eea7f68ccdc5c37352f80a3a2cc6fd6338eb
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2023-03-17 13:02:13 +0300

    first part of the homework

---
+++ C_sharp_Lections/Seminar7_Homework/DoubleArrayMN/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными
+// вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+int Prompt(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double [,] CreateFillPrintArray(int m, int n)
+{
+    double [,] array = new double [m,n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(-100,101) + Math.Round(new Random().NextDouble(),2);
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    return array;
+}
+int m = Prompt("Введите количество строк");
+int n = Prompt("Введите количество столбцов");
+CreateFillPrintArray(m,n);
---
+++ C_sharp_Lections/Seminar7_Homework/task1/Program.cs
@@ -0,0 +1,50 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+int Prompt(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int find = Prompt("Введите искомое значение в интервале от -20 до 20");
+int index0 = 0;
+int index1 = 1;
+int count = 0;
+
+int [,] CreateFillOutputArray(int m, int n, int find, ref int count)
+{
+    int [,] array = new int [m,n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(-20,21);
+            if (array[i,j] == find)
+            {
+               index0 = i;
+               index1 = j; 
+               count = 1;
+            }
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    return array;
+}
+
+int m = Prompt("Введите количество строк");
+int n = Prompt("Введите количество столбцов");
+CreateFillOutputArray(m,n,find,ref count);
+if (count == 0)
+{
+    Console.WriteLine($"Искомый элемент {find} в массиве случайных чисел не найден");
+}
+else
+{
+    Console.WriteLine($"Искомый элемент {find} найден в массиве и находится под индексами ({index0}, {index1})");
+}
+
