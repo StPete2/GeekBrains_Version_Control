c7b2823863807ac72abacd1c44caa9d9c1bad087
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2023-03-18 10:40:03 +0300

    first part day 2

--- C_sharp_Lections/Seminar7_Homework/task2/Program.cs
+++ C_sharp_Lections/Seminar7_Homework/task2/Program.cs
@@ -5,3 +5,29 @@
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+int Prompt(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int [,] CreateOutputArray(int m, int n)
+{
+    int [,] array = new int [m,n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(1,11);
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    return array;
+}
+int m = Prompt("Введите количество строк");
+int n = Prompt("Введите количество столбцов");
+
+//CreateOutputArray(m,n);
+Console.WriteLine();
+int [,] array = CreateOutputArray(m,n);
