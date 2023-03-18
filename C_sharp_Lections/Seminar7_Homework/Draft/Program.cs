c1d7b083c301468d22c204bb7ebf9bd693c97891
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2023-03-18 11:15:49 +0300

    second part done

--- C_sharp_Lections/Seminar7_Homework/Draft/Program.cs
+++ C_sharp_Lections/Seminar7_Homework/Draft/Program.cs
@@ -1,11 +1,4 @@
-﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое 
-// элементов в каждом столбце.
-// Например, задан массив:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
-// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
-int Prompt(string text)
+﻿int Prompt(string text)
 {
     Console.WriteLine(text);
     return Convert.ToInt32(Console.ReadLine());
@@ -14,16 +7,18 @@ int Prompt(string text)
 int [,] CreateOutputArray(int m, int n)
 {
     int [,] array = new int [m,n];
+    int [,] newArray = new int [m,n];
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
             array[i,j] = new Random().Next(1,11);
+            newArray[i,j] = array [i,j];
             Console.Write(array[i,j] + "\t");
         }
         Console.WriteLine();
     }
-    return array;
+    return newArray;
 }
 int m = Prompt("Введите количество строк");
 int n = Prompt("Введите количество столбцов");
