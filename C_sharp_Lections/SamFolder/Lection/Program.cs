edecb7c5f483cc2fb8beb9e620da1a63ed90a3aa
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2023-03-19 13:09:59 +0300

    first part of the lection7

---
+++ C_sharp_Lections/SamFolder/Lection/Program.cs
@@ -0,0 +1,14 @@
+﻿string caption = "Intensive C# Demo text";
+int screenWidthPosition = (Console.WindowWidth - caption.Length)/2;
+int screenHeightPosition = Console.WindowHeight/2;
+void DrawText (string text, int left, int top)
+{
+    Console.SetCursorPosition(left,top);
+    Console.WriteLine(text);
+}
+DrawText (caption, screenWidthPosition, screenHeightPosition);
+DrawText(
+    text: caption,
+    left: screenWidthPosition,
+    top: screenHeightPosition
+);
--- C_sharp_Lections/SamFolder/Misc/Program.cs
+++ C_sharp_Lections/SamFolder/Misc/Program.cs
@@ -1,14 +1,23 @@
-﻿bool IsEven(int value)
-{
-    return value%2 ==0;
-}
-int Prompt ()
-{
-    Console.WriteLine("Введите число для проверки деления на 2");
-    return Convert.ToInt32(Console.ReadLine());
-}
-int a = Prompt();
-bool result = IsEven(a);
-Console.WriteLine(result);
+﻿// bool IsEven(int value)
+// {
+//     return value%2 ==0;
+// }
+// int Prompt ()
+// {
+//     Console.WriteLine("Введите число для проверки деления на 2");
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+// int a = Prompt();
+// bool result = IsEven(a);
+// Console.WriteLine(result);
 
-Console.WriteLine(a.GetType().Name);
+// Console.WriteLine(a.GetType().Name);
+
+Console.WriteLine(
+    "Это пример длинного текста",
+    "Это продолжение примера длинного текста"
+);
+// Console.WriteLine(
+//     $"пример длинного длинного текста",
+//     "Пример длинного длинного текста"
+// );
