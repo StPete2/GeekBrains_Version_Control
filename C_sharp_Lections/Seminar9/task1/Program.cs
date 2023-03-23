2640c678611773d12d421d8b94a591c8c1e4ebd6
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2023-03-23 15:43:03 +0300

    seminar9

---
+++ C_sharp_Lections/Seminar9/task1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и 
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+// int a = 453;
+// int sum = 0;
+// int RecSumNumbers(int a)
+// {
+//     if (a==0)
+//     {
+//         return sum;
+//     }
+//     else
+//     {
+//         sum = sum + a%10;
+//         RecSumNumbers(a/10);
+//         return sum;
+//     }
+// }
+// RecSumNumbers(a);
+// Console.WriteLine(sum);
+
+int RecSumNumbers(int a)
+{
+    if (a == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return a%10 + RecSumNumbers(a/10);
+    }
+}
+Console.WriteLine(RecSumNumbers(124));
