406b3fd248eae11a428b07c45e5ffd25e6020f38
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2023-03-31 13:08:39 +0300

    friday

---
+++ C_sharp_Lections/LeetCode/1342.NumberOfSteps/Program.cs
@@ -0,0 +1,22 @@
+﻿int numberOfSteps(int num)
+{
+    int count = 0;
+    while (num > 0)
+    {
+        if (num %2 == 0)
+        {
+            num = num / 2;
+            count++;
+        }
+        else if (num % 2 != 0)
+        {
+            num = num - 1;
+            count++;
+        }
+    }   
+    return count;
+}
+Console.WriteLine("Input integer");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = numberOfSteps(num);
+Console.WriteLine(count);
