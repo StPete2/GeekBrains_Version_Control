c9a7cfddb0fd72118b068ecfa107352665c3fd69
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2023-04-05 12:03:19 +0300

    part 1

--- C_sharp_Lections/SamFolder/task1/Program.cs
+++ C_sharp_Lections/SamFolder/task1/Program.cs
@@ -1,1 +1,29 @@
 ﻿// Pascal triangle
+int Prompt(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int n = Prompt("Введите количество строк в треугольнике");
+Console.WriteLine();
+
+int [,] matrix = new int [n,n];
+matrix[0,0]=1;
+Console.WriteLine(matrix[0,0]);
+
+for (int i = 1; i < n; i++)
+{
+    for (int j = 0; j <= i; j++)
+    {
+        if ( j==0 || i==j)
+        {
+            matrix [i,j] = 1;
+        }
+        else
+        {
+            matrix[i,j] = matrix[i-1,j] + matrix[i-1,j-1];
+        }
+        Console.Write(matrix[i,j]+ "\t");
+    }
+    Console.WriteLine();
+}
