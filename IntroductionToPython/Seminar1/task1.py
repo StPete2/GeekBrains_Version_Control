3afae5e9bbc381fb6ee02b75558b4634ae6bf507
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2023-05-23 15:50:12 +0300

    Seminar1 Homework ready

--- IntroductionToPython/Seminar1/task1.py
+++ IntroductionToPython/Seminar1/task1.py
@@ -1,4 +1,4 @@
-a = 2016
+a = 2011
 res = a %4 == 0 and a %100 !=0
 res2 = a%400 == 0
 resFinal = res or res2
@@ -8,7 +8,7 @@ if resFinal:
 else:
     print('No')
 
-res3 = 'YES' if a%4 == 0 and a %100 !=0 or a %400 == 0 else 'NO'
-print(res3)
+# res3 = 'YES' if a%4 == 0 and a %100 !=0 or a %400 == 0 else 'NO'
+# print(res3)
 
-print('YES!') if a%4 ==0 and a %100!=0 or a %400 ==0 else 'No!'
+print('YES!' if a%4 ==0 and a %100!=0 or a %400 ==0 else 'No!')
