9332c0dbf002639a975605b9af9d853c5d624043
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2023-06-09 12:01:50 +0300

    Seminar7

---
+++ C_sharp_Lections/LeetCode/MergeSortedArrayLong/Program.cs
@@ -0,0 +1,22 @@
+﻿            // int minPosition = 0;
+            // int temp = 0;
+            // for (int i = 0; i < n; i++)
+            // {
+            //     nums1[m+i] = nums2[i];
+            // }
+            // for (int i = 0; i < m+n-1; i ++)
+            // {
+            //     minPosition = i;
+            //     for (int j = i + 1; j < m+n; j ++)
+            //     {
+            //         if (nums1[minPosition]>nums1[j])
+            //         {
+            //             minPosition = j;
+            //         }
+            //     }
+            //     if (minPosition !=i)
+            //     {
+            //         temp = nums1[i];
+            //         nums1[i] = nums1[minPosition];
+            //         nums1[minPosition] = temp;
+            //     }
