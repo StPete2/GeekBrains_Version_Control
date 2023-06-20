0b813834db37a84255517c2bf0bad40b31716641
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2023-06-20 04:36:21 +0300

    test commit 2

--- C_sharp_Lections/LeetCode/FindDisappearedNumbers/Program.cs
+++ C_sharp_Lections/LeetCode/FindDisappearedNumbers/Program.cs
@@ -21,22 +21,22 @@ IList<int> FindDisappearedNumbers(int[] nums)
     Console.WriteLine(String.Join(" ", nums));
     List<int> resultList = new List<int>();
     int k = 1;
-    if (nums[0] != k)
-    {
-        for (int n = 1; n < nums[0]; n++)
-        {
-            resultList.Add(k);                      
-            k++;
-        }
-    }    
-    k++;
-    for (int i = 1; i < nums.Length; i++)
+    // if (nums[0] != k)
+    // {
+    //     for (int n = 1; n < nums[0]; n++)
+    //     {
+    //         resultList.Add(k);                      
+    //         k++;
+    //     }
+    // }    
+    // k++;
+    for (int i = 0; i < nums.Length; i++)
     {
         if (nums[i] != k)
         {
-            if (nums[i] == nums[i - 1] || nums[i]<k)
+            if (nums[i]<k)
             {
-                for (int j = i; j < nums.Length; j++)
+                for (int j = i+1; j < nums.Length; j++)
                 {
                     if (nums[j]==k)
                     {
@@ -53,19 +53,26 @@ IList<int> FindDisappearedNumbers(int[] nums)
                     }
                 }              
             }
-            else
+            else if (nums[i]>k)
             {
                 for (int l = k; l < nums[i]; l++)
                 {
                     resultList.Add(k);                    
                     Console.WriteLine($"l {l}, nums[i] {nums[i]}");                                        
                     k++;
-                }                 
-                k++;                            
-            }            
+                    if (nums[i]==k)
+                    {
+                        k++;    
+                    }                        
+                }                                 
+            }
         }
         else
         {
+            if (i == nums.Length-1 && nums[i]==k && k!=nums.Length)
+            {
+                resultList.Add(k+1);
+            }
             k++;
         }
     }
@@ -77,5 +84,5 @@ int [] inputData3 = new int[]{1,1,1,1,2,2,2,2};
 int [] inputData4 = new int []{5,4,6,7,9,3,10,9,5,6};
 int [] inputData10 = new int []{27,40,6,21,14,36,10,19,44,10,41,26,39,20,25,19,14,7,29,27,40,38,11,44,4,6,48,39,9,13,7,45,41,23,31,8,24,1,3,5,28,11,49,29,18,4,38,32,24,15};
 int []inputData7 = new int []{1,2,2,2,4,5};
-IList<int> result = FindDisappearedNumbers(inputData7);
+IList<int> result = FindDisappearedNumbers(inputData4);
 Console.WriteLine(String.Join(" ", result));
