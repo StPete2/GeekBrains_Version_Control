3b32228f8403e2e3dacb8848eab99eb0cc8cd542
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2023-06-20 03:47:39 +0300

    First part

--- C_sharp_Lections/LeetCode/FindDisappearedNumbers/Program.cs
+++ C_sharp_Lections/LeetCode/FindDisappearedNumbers/Program.cs
@@ -17,17 +17,65 @@ IList<int> FindDisappearedNumbers(int[] nums)
         temp = nums[i];
         nums[i] = nums[minPosition];
         nums[minPosition] = temp;
-    }
+    }    
+    Console.WriteLine(String.Join(" ", nums));
     List<int> resultList = new List<int>();
-    for (int i = 1; i <= nums.Length; i++)
+    int k = 1;
+    if (nums[0] != k)
     {
-        if (nums[i] != i)
+        for (int n = 1; n < nums[0]; n++)
         {
-            resultList.Add(i);
+            resultList.Add(k);                      
+            k++;
+        }
+    }    
+    k++;
+    for (int i = 1; i < nums.Length; i++)
+    {
+        if (nums[i] != k)
+        {
+            if (nums[i] == nums[i - 1] || nums[i]<k)
+            {
+                for (int j = i; j < nums.Length; j++)
+                {
+                    if (nums[j]==k)
+                    {
+                        k++;
+                        break;
+                    }
+                }
+                if (i == nums.Length-1)
+                {
+                    for (int m = k; m < nums.Length+1; m++)
+                    {
+                        resultList.Add(k);
+                        k++;
+                    }
+                }              
+            }
+            else
+            {
+                for (int l = k; l < nums[i]; l++)
+                {
+                    resultList.Add(k);                    
+                    Console.WriteLine($"l {l}, nums[i] {nums[i]}");                                        
+                    k++;
+                }                 
+                k++;                            
+            }            
+        }
+        else
+        {
+            k++;
         }
     }
     return resultList;
 }
 int [] inputData = new int []{4,3,2,7,8,2,3,1};
-IList<int> result = FindDisappearedNumbers(inputData);
+int [] inputData2 = new int[]{1,1,1,1,1,1,1,1};
+int [] inputData3 = new int[]{1,1,1,1,2,2,2,2};
+int [] inputData4 = new int []{5,4,6,7,9,3,10,9,5,6};
+int [] inputData10 = new int []{27,40,6,21,14,36,10,19,44,10,41,26,39,20,25,19,14,7,29,27,40,38,11,44,4,6,48,39,9,13,7,45,41,23,31,8,24,1,3,5,28,11,49,29,18,4,38,32,24,15};
+int []inputData7 = new int []{1,2,2,2,4,5};
+IList<int> result = FindDisappearedNumbers(inputData7);
 Console.WriteLine(String.Join(" ", result));
