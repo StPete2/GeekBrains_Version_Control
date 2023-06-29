2178a5956ec132e1590d37ff247c84b3876557fc
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2023-06-29 12:17:30 +0300

    Main part of the homework seminar1 ready

---
+++ JavaAndTheBasicAPI/Seminar1_Homework/Task2.java
@@ -0,0 +1,29 @@
+// 2) Вывести все простые числа от 1 до 1000
+
+package Seminar1_Homework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int n = 1000;
+        System.out.println(findSimpleNumbers(n));
+
+    }
+    private static List<Integer> findSimpleNumbers(int maxNumber){
+        List <Integer> result = new ArrayList<>();
+        for (int i = 1; i <= maxNumber; i++) {
+            boolean simpleNumber = true;
+            for (int j = 2; j <= i/2; j++) {
+                if (i % j ==0) {
+                    simpleNumber = false;
+                }
+            }
+            if (simpleNumber) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}
---
+++ JavaAndTheBasicAPI/Seminar1_Homework/Task3.java
@@ -0,0 +1,52 @@
+// 3) Реализовать простой калькулятор
+package Seminar1_Homework;
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int x = firstNumber();
+        int y = secondNumber();
+        String str = getSign();
+        switch (str) {
+            case "+":
+                System.out.printf("%d + %d = %d", x, y, x+y);
+                break;
+            case "-":
+                System.out.printf("%d - %d = %d", x, y, x-y);
+                break;
+            case "*":
+                System.out.printf("%d * %d = %d", x, y, x*y);
+                break;
+            case "/":
+                System.out.printf("%d / %d = %d", x, y, x/y);
+                break;
+            default:
+                System.out.println("Вы ввели некоректный знак. Программа завершает свою работу");
+                break;
+        }
+
+    }
+    public static int firstNumber(){
+        Scanner iScanner = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        int number = iScanner.nextInt();
+        // iScanner.close();
+        return number;
+    }
+
+    public static int secondNumber(){
+        Scanner iScanner = new Scanner(System.in);
+        System.out.print("Введите второе число: ");
+        int number = iScanner.nextInt();
+        // iScanner.close();
+        return number;
+    }
+
+    public static String getSign(){
+        Scanner iScanner = new Scanner(System.in);
+        System.out.print("Введите арифметический знак + , -, *, /: ");
+        String str = iScanner.nextLine();
+        iScanner.close();
+        return str;
+    }
+}
