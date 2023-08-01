646084e8f5df9aba89bce80427935bb7c4fe83b6
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2023-08-01 12:17:11 +0300

    Seminar4 added

---
+++ JavaAndTheBasicAPI/OOP_Seminar4/Worker.java
@@ -0,0 +1,9 @@
+package OOP_Seminar4;
+
+// public class Worker <T>{
+//     private T id;
+
+//     public Worker <T> getById(T id){
+//         return this;
+//     }
+// }
---
+++ JavaAndTheBasicAPI/OOP_Seminar4/misc.java
@@ -0,0 +1,19 @@
+// файл для самоподготовки
+
+package OOP_Seminar4;
+
+// import java.util.Collection;
+// import java.util.Iterator;
+
+public class misc {
+    public static void main(String[] args) {
+
+        // void printCollection(Collection<E> c){
+        //     Iterator<E> i = c.iterator();
+        //     for (int k = 0; k < c.zise(); j++) {
+        //         System.out.println(i.next());
+        //     }
+        // }
+
+    }
+}
