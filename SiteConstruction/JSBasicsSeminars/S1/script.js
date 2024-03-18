079614398e2c19c9dfc5c06d577e901e4e5d5d60
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2024-03-18 17:54:54 +0300

    JSBasics L1 and S1 part

---
+++ SiteConstruction/JSBasicsSeminars/S1/script.js
@@ -0,0 +1,26 @@
+console.log('Текст из script.js!');
+
+const NUM = 123;
+console.log(NUM);
+let a = 5;
+a = 7;
+console.log(a);
+
+let b;
+b = 1 + 2 + 3; 
+console.log(b);
+b = 'text';
+console.log(b);
+/* comments */
+
+// const myAge = +prompt("Enter your Age: ");
+// console.log(typeof myAge);
+// const currentYear = 2024;
+// const sumMyAgeAndCurrentYear = myAge + currentYear;
+
+// console.log(myAge, currentYear);
+// console.log(sumMyAgeAndCurrentYear);
+const str = '123.2';
+console.log(str);
+console.log(+str);
+console.log(Number(str));
