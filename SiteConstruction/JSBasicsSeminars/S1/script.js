732283ea4327e657880b170ee144ebddd326433f
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2024-03-19 13:05:24 +0300

    test final

--- SiteConstruction/JSBasicsSeminars/S1/script.js
+++ SiteConstruction/JSBasicsSeminars/S1/script.js
@@ -6,21 +6,58 @@ let a = 5;
 a = 7;
 console.log(a);
 
-let b;
-b = 1 + 2 + 3; 
-console.log(b);
-b = 'text';
-console.log(b);
+// let b;
+// b = 1 + 2 + 3; 
+// console.log(b);
+// b = 'text';
+// console.log(b);
 /* comments */
 
 // const myAge = +prompt("Enter your Age: ");
+// const myAge2 = Number.parseInt(prompt("Второй возраст: "));
 // console.log(typeof myAge);
-// const currentYear = 2024;
+const currentYear = 2024;
 // const sumMyAgeAndCurrentYear = myAge + currentYear;
+// console.log(myAge2);
 
 // console.log(myAge, currentYear);
 // console.log(sumMyAgeAndCurrentYear);
-const str = '123.2';
-console.log(str);
-console.log(+str);
-console.log(Number(str));
+// const str = '12a3.2';
+// console.log(str);
+// console.log(+str);
+// console.log(Number(str));
+// console.log(Number.parseInt(str));
+// console.log(Number.parseFloat(str));
+
+let firstExample = 5 + 5 * 3 + 3;
+console.log(firstExample);
+
+const secondVar = 0.1;
+const thirdVar = 0.2;
+const sumSVTV = secondVar + thirdVar;
+//округление числа, превращая его в строку
+// console.log(sumSVTV.toFixed(1));
+// const toFixedVar = +sumSVTV.toFixed(1);
+// console.log(typeof(toFixedVar));
+
+//округляет всегда до целого числа по правилам округления: свыше .5 - вверх, ниже 0.5 - вниз
+console.log(Math.round(sumSVTV));
+
+//округляет в меньшую сторону
+console.log(Math.floor(sumSVTV));
+
+//всегда вверх
+console.log(Math.ceil(sumSVTV));
+
+//убирает дробную часть
+console.log(Math.trunc(sumSVTV));
+
+Math.round(sumSVTV * 10) / 10;
+
+const firstName = 'Aleksey';
+const lastName = 'Eryukhin';
+console.log(`${firstName} ${lastName}`);
+
+console.log(`${firstName} ${lastName}`.toLowerCase());
+
+console.log(`${firstName} ${lastName}`.toLocaleUpperCase());
