8de3008364ec32a4ed09c0003f9722fb12052413
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2024-04-01 17:28:29 +0300

    l4 and s4 part

---
+++ SiteConstruction/JSBasics/L4/1.js
@@ -0,0 +1,18 @@
+// const number = 5;
+// let i = 1;
+// while (i <= number) {
+//     console.log(i++);
+// }
+
+for (i = 0; i < 3; i++) {
+    console.log(i);
+}
+
+const arr = [1, 2, 'hello', 5, 'Go-Go!'];
+console.log(arr);
+console.log(arr[2]);
+
+console.log(arr.indexOf(2));
+
+const arr2 = arr.slice(1, 5);
+console.log(arr2);
