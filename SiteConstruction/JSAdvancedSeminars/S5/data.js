34b4bd2cdc56e616ba5eb0f7a81f40f1897bf5b0
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2024-04-26 12:19:59 +0300

    s5 part

---
+++ SiteConstruction/JSAdvancedSeminars/S5/data.js
@@ -0,0 +1,28 @@
+const dataInfo = `
+[
+    {
+        "class" : "img__container",
+        "id" : "ef6",
+        "url" : "https://as2.ftcdn.net/v2/jpg/04/34/04/93/1000_F_434049374_A9YcF67iDB4nUxvrSUn4diw2UQTxj6lk.jpg",
+        "width" : 500,
+        "height" : 334,
+        "текст" : "Картинка № 1"
+    },
+    {
+        "class" : "img__container",
+        "id" : "bpj",
+        "url" : "https://as1.ftcdn.net/v2/jpg/03/36/58/24/1000_F_336582491_3oNBArWasescAb0asVKl6NYtRTfTAUt3.jpg",
+        "width" : 796,
+        "height" : 1061,
+        "текст" : "Картинка № 2"
+    },
+    {
+        "class" : "img__container",
+        "id" : "dt7",
+        "url" : "https://as2.ftcdn.net/v2/jpg/02/71/31/03/1000_F_271310317_b1bhgCiN0zISRnLtEq5Vs9Nq3su7gU17.jpg",
+        "width" : 765,
+        "height" : 1024,
+        "текст" : "Картинка № 3"
+    }
+]
+`;
