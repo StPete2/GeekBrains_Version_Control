d1d26109a8e2015df93dd05d05d71adec8dc961b
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2024-04-29 11:11:27 +0300

    sem6 part

---
+++ SiteConstruction/JSAdvancedSeminars/S6/data.js
@@ -0,0 +1,47 @@
+const dataProductsSem = `[
+    {
+        "class": "cart-item",
+        "imgSrc": "./img/cart-item__img1.jpg",
+        "alt": "photo of a product",
+        "h2FirstLine": "MANGO PEOPLE",
+        "h2SecondLine": "T-SHIRT",
+        "price": "$300",
+        "color": "Red",
+        "size": "XL",
+        "inputValue": "2"
+    },
+    {
+        "class": "cart-item",
+        "imgSrc": "./img/cart-item__img2.jpg",
+        "alt": "photo of a product",
+        "h2FirstLine": "MANGO PEOPLE2",
+        "h2SecondLine": "T-SHIRT",
+        "price": "$100",
+        "color": "Red",
+        "size": "XL",
+        "inputValue": "3"
+    }
+]`;
+
+const dataProducts = [
+    {
+        imgSrc: "./img/cart-item__img1.jpg",
+        alt: "photo of a product",
+        h2FirstLine: "MANGO PEOPLE",
+        h2SecondLine: "T-SHIRT",
+        price: "$300",
+        color: "Red",
+        size: "XL",
+        inputValue: "2"
+    },
+    {
+        imgSrc: "./img/cart-item__img2.jpg",
+        alt: "photo of a product",
+        h2FirstLine: "MANGO PEOPLE",
+        h2SecondLine: "T-SHIRT",
+        price: "$300",
+        color: "Red",
+        size: "XL",
+        inputValue: "2"
+    }
+]
---
+++ SiteConstruction/JSAdvancedSeminars/S6/script.js
@@ -0,0 +1,5 @@
+const dataJs = JSON.parse(dataProductsSem);
+console.log(dataJs);
+
+const dataJs2 = JSON.parse(dataProducts);
+console.log(dataJs2);
