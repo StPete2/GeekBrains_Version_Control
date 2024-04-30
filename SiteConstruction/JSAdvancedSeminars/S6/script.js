bd5a87a11045eb240ad2c6d9849e85ddc55ca533
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2024-04-30 15:06:23 +0300

    hw6 1st

--- SiteConstruction/JSAdvancedSeminars/S6/script.js
+++ SiteConstruction/JSAdvancedSeminars/S6/script.js
@@ -10,11 +10,11 @@ dataJs.forEach(element => {
     const cloneEl = templateEl.content.cloneNode(true);
 
     cloneEl.querySelector('img.cart-item__img').src = element.imgSrc;
-    cloneEl.querySelector('h2.cart-item__heading').innerHtml = element.h2FirstLine;
-    cloneEl.querySelector('h2.cart-item__heading_second_line').innerHtml = element.h2SecondLine;
-    cloneEl.querySelector('span.cart-item__attribute').innerHtml = element.price;
-    cloneEl.querySelector('span.cart-item__attribute-meaning').innerHtml = element.color;
-    cloneEl.querySelector('span.cart-item__attribute-meaning-size').innerHtml = element.size;
+    cloneEl.querySelector('h2.cart-item__heading').innerHTML = element.h2FirstLine;
+    cloneEl.querySelector('h2.cart-item__heading_second_line').innerHTML = element.h2SecondLine;
+    cloneEl.querySelector('span.cart-item__attribute').innerHTML = element.price;
+    cloneEl.querySelector('span.cart-item__attribute-meaning').innerHTML = element.color;
+    cloneEl.querySelector('span.cart-item__attribute-meaning-size').innerHTML = element.size;
     cloneEl.querySelector('#cart-item__quantity').value = element.inputValue;
     cartItemsBox.appendChild(cloneEl);
 });
