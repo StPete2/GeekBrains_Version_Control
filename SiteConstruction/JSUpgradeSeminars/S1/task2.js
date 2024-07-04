7d4373e0aa73bf4ff8f0db6f8ab2067ba7b1bfe7
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2024-07-04 13:12:08 +0300

    s1 continue

--- SiteConstruction/JSUpgradeSeminars/S1/task2.js
+++ SiteConstruction/JSUpgradeSeminars/S1/task2.js
@@ -10,3 +10,59 @@ const books = [
     { title: "Brave New World", author: "Aldous Huxley" },
     { title: "Fahrenheit 451", author: "Ray Bradbury" },
 ];
+
+const library = {
+    // books: books,
+    books,
+    // [Symbol.iterator]: function* () {
+    //     for (const book of this.books) {
+    //         yield book;
+    //     }
+    // }
+
+    *[Symbol.iterator]() {
+        // функция-генератор, которая возвращает не одно значение
+        for (const book of this.books) {
+            yield book; // продолжает работу после вызова первой книги! в отличие от return
+        }
+    },
+};
+
+// for (const book of library) {
+//     console.log(book);
+// }
+
+const iterator = library[Symbol.iterator]();
+console.log(iterator);
+
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
+
+const library2 = {
+    books: books,
+    [Symbol.iterator]() {
+        let index = 0;
+        return {
+            // next:() => { ещё один вариант - через стрелочную функция. Стрелочная функция всегда запоминает окружение и будет работать через this.books.lenght
+            next() {
+                if (index < books.length) {
+                    return {
+                        value: books[index++],
+                        done: false,
+                    };
+                }
+                return {
+                    value: undefined,
+                    done: true,
+                };
+            },
+        };
+    },
+};
+
+for (const book of library2) {
+    console.log(book);
+}
